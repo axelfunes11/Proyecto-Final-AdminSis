cfcc245dd86b5212704899f8f1ad30e40153188b
Axel Funes <faxelemmanuel@gmail.com>
2026-07-13 00:27:57 -0600

    Configuración despliegue final con NGINX y systemd

--- frontend/angular-app/angular-app/src/environments/environments.ts
+++ frontend/angular-app/angular-app/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  usersApiUrl: 'http://localhost:3001/api/users',
-  inventoryApiUrl: 'http://localhost:3002/api/inventory'
+  usersApiUrl: '/api/users',
+  inventoryApiUrl: '/api/inventory'
 };
